b56de68fb970b9631b7c212c23997044bb7b0c1f
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-04-29 13:22:34 +0600

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template_string
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    html = f'''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AI - ChatBot</title>
+        <style>
+            body {{
+                background-image: url('https://images.unsplash.com/photo-1470115636492-6d2b56f9146d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=800&ixid=MnwxfDB8MXxyYW5kb218MHx8d2FsbHBhcGVyfHx8fHx8MTY4MjY2OTA1MQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=800');
+                background-size: cover;
+            }}
+            .commands-container {{
+                border-radius: 25px;
+                background-color: rgba(241, 241, 241, 0.8);
+                padding: 20px;
+                width: 60%;
+                margin: 0 auto;
+            }}
+            .command {{
+                animation: typing 2s steps(1000000, end), blink-caret .75s step-end infinite;
+                font-family: monospace;
+                font-size: 16px;
+                color: #000000;
+                text-shadow: 0 0 3px #fabb34;
+                white-space: nowrap;
+                overflow: hidden;
+            }}
+            @keyframes typing {{
+                from {{ width: 0 }}
+                to {{ width: 100% }}
+            }}
+            @keyframes blink-caret {{
+                50% {{ border-color: transparent }}
+            }}
+            h1 {{
+                text-align: center;
+                color: white;
+            }}
+            h2 {{
+                text-align: center;
+            }}
+        </style>
+    </head>
+    <body>
+        <h1>Created by Mishal</h1>
+        <div class="commands-container">
+            <h2>HaruniAI Commands</h2>
+            <p class="command">!pfp [image_url] - Change the bot's profile picture</p>
+            <p class="command">!changeusr [new_username] - Change the bot's username</p>
+            <p class="command">!invite_all - Generate invites for all servers the bot is in</p>
+            <p class="command">!addroles - Add all roles to the specified user</p>
+            <p class="command">!checkperm - Check the bot's permissions in this server</p>
+            <p class="command">!troll - Change all nicknames to 'Slaves lol' for trolzez</p>
+            <p class="command">!ping - Pong</p>
+            <p class="command">!addchannel - Add the current channel to the list of active channels</p>
+            <p class="command">!randomstory - Uses AI to generate a random story</p>
+            <p class="command">!define [Word] - Uses AI to explain a word</p>
+            <p class="command">!ask [Question] - Uses AI to answer a question</p>
+            <p class="command">!storyonusr [@USER] - Uses AI to create a random story on a mentioned @user</p>
+            <div style="text-align: center; margin-top: 20px;">
+                <a href="https://github.com/mishalhossin/Gpt3-chatbot-discord" target="_blank" style="display: inline-block; padding: 10px 20px; background-color: #fabb34; color: #fff; border-radius: 50px;">View Source Code on GitHub</a>
+            </div>
+        </div>
+    </body>
+    </html>
+    '''
+
+    return render_template_string(html)
+
+def run():
+    app.run(host='0.0.0.0', port=3000)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
---
+++ main.py
@@ -0,0 +1,87 @@
+import os
+import phind
+import aiohttp
+import discord
+from keep_alive import keep_alive
+from discord.ext import commands
+
+# Set up the Discord bot
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    global current_status
+    print(f"{bot.user.name} has connected to Discord!")
+
+
+def generate_response(prompt):
+    result = phind.Completion.create(
+        model='gpt-4',
+        prompt=prompt,
+        results=phind.Search.create(prompt, actualSearch=False),  # create search (set actualSearch to False to disable internet)
+        creative=False,
+        detailed=False,
+        codeContext=''  # up to 3000 chars of code
+    )
+    return result.completion.choices[0].text
+
+message_history = {}
+MAX_HISTORY = 4
+
+
+@bot.event
+async def on_message(message):
+    if isinstance(message.channel, discord.DMChannel):
+        
+        author_id = str(message.author.id)
+        if author_id not in message_history:
+            message_history[author_id] = []
+        message_history[author_id].append(message.content)
+        message_history[author_id] = message_history[author_id][-MAX_HISTORY:]
+        
+        user_prompt = "\n".join(message_history[author_id])
+        prompt = f"{user_prompt}\n{message.author.name}: {message.content}\n{bot.user.name}:"
+        response = generate_response(prompt)
+
+        # get user object from ID
+        user = bot.get_user(int(author_id))
+        if user:
+            await message.reply(response)
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def pfp(ctx, attachment_url=None):
+    if attachment_url is None and not ctx.message.attachments:
+        return await ctx.send(
+            "Please provide an image URL or attach an image with the command"
+        )
+
+    if attachment_url is None:
+        attachment_url = ctx.message.attachments[0].url
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(attachment_url) as response:
+            await bot.user.edit(avatar=await response.read())
+
+    await ctx.send("My profile picture has been updated!")
+
+@bot.command()
+async def ping(ctx):
+    latency = bot.latency * 1000
+    await ctx.send(f"Pong! Latency: {latency:.2f} ms")
+
+@bot.command()
+async def changeusr(ctx, new_username):
+    # Check that the new username is not already taken
+    taken_usernames = [user.name.lower() for user in bot.get_all_members()]
+    if new_username.lower() in taken_usernames:
+        await ctx.send(f"Sorry, the username '{new_username}' is already taken.")
+        return
+
+
+keep_alive()
+
+bot.run(os.getenv("DISCORD_TOKEN"))
