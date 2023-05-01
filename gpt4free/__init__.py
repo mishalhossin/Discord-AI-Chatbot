95b2149713751b334a2d97ddad553f495acfa44c
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-05-02 05:56:23 +0600

    Update __init__.py

--- gpt4free/__init__.py
+++ gpt4free/__init__.py
@@ -6,12 +6,7 @@ from gpt4free import theb
 class Provider(Enum):
     """An enum representing  different providers."""
 
-    You = 'you'
-    Poe = 'poe'
-    ForeFront = 'fore_front'
     Theb = 'theb'
-    CoCalc = 'cocalc'
-    UseLess = 'useless'
 
 
 class Completion:
@@ -28,41 +23,11 @@ class Completion:
         :param kwargs:  Additional keyword arguments to pass to the provider while invoking
         :return: A string representing the response from the provider
         """
-        if provider == Provider.Poe:
-            return Completion.__poe_service(prompt, **kwargs)
-        elif provider == Provider.You:
-            return Completion.__you_service(prompt, **kwargs)
-        elif provider == Provider.ForeFront:
-            return Completion.__fore_front_service(prompt, **kwargs)
-        elif provider == Provider.Theb:
+        if provider == Provider.Theb:
             return Completion.__theb_service(prompt, **kwargs)
-        elif provider == Provider.CoCalc:
-            return Completion.__cocalc_service(prompt, **kwargs)
-        elif provider == Provider.UseLess:
-            return Completion.__useless_service(prompt, **kwargs)
         else:
             raise Exception('Provider not exist, Please try again')
-        
-    @staticmethod
-    def __useless_service(prompt: str, **kwargs) -> str:
-        return usesless.Completion.create(prompt = prompt, **kwargs)
-
-    @staticmethod
-    def __you_service(prompt: str, **kwargs) -> str:
-        return you.Completion.create(prompt, **kwargs).text
-
-    @staticmethod
-    def __poe_service(prompt: str, **kwargs) -> str:
-        return quora.Completion.create(prompt=prompt, **kwargs).text
-
-    @staticmethod
-    def __fore_front_service(prompt: str, **kwargs) -> str:
-        return forefront.Completion.create(prompt=prompt, **kwargs).text
 
     @staticmethod
     def __theb_service(prompt: str, **kwargs):
         return ''.join(theb.Completion.create(prompt=prompt))
-
-    @staticmethod
-    def __cocalc_service(prompt: str, **kwargs):
-        return cocalc.Completion.create(prompt, cookie_input=kwargs.get('cookie_input', '')).text
