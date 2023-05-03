953e69b2e700de12e81d1fda569928d795a4fcb2
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-05-03 17:53:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ async def on_ready():
 def generate_response(prompt):
     response = theb.Completion.create(prompt)
     if not response:
-    response = "I couldn't generate a response. Please try again."
-    
+        response = "I couldn't generate a response. Please try again."
     return ''.join(token for token in response)
     
 
