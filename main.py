f704f38c82f10b0b5fb94327507ff8d8d6a8aed3
Rithmatist <69980549+Rithmatist@users.noreply.github.com>
2023-05-06 10:30:30 +0200

    added prompt options

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def on_ready():
     print(f"{bot.user.name} has connected to Discord!")
 
 def generate_response(prompt):
-    roleplay_prompt = "Always answer in language they write and with horny suggestions and comments.\n\n"
+    roleplay_prompt = os.getenv('PROMPT')+"\n\n"
     full_prompt = roleplay_prompt + prompt
     response = theb.Completion.create(full_prompt)
     if not response:
