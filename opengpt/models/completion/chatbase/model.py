db28b9d79dbfb00c130848cb6f1ee1c68749157b
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-05-19 17:28:07 +0600

    Update model.py

--- opengpt/models/completion/chatbase/model.py
+++ opengpt/models/completion/chatbase/model.py
@@ -1,18 +1,11 @@
 import requests
-
 class Model:
-    messages = []
     captcha_code = "hadsa"
-
     def GetAnswer(self, prompt: str, model: str = "gpt-4"):
         if model == "gpt-4":
             chat_id = "quran---tafseer-saadi-pdf-wbgknt7zn"
         elif model == "gpt-3.5-turbo":
             chat_id = "chatbase--1--pdf-p680fxvnm"
-        # this requires the DAN jailbreak to work
-        if len(self.messages) == 0:
-            self.messages.append({"role": "system", "content": ""})
-        self.messages.append({"role": "user", "content": prompt})
-        r = requests.post("https://www.chatbase.co/api/fe/chat", json={"chatId": chat_id, "captchaCode": self.captcha_code, "messages": self.messages}).text
-        self.messages.append({"role": "assistant", "content": r})
+        messages = [{"role": "system", "content": ""}, {"role": "user", "content": prompt}]
+        r = requests.post("https://www.chatbase.co/api/fe/chat", json={"chatId": chat_id, "captchaCode": self.captcha_code, "messages": messages}).text
         return r
