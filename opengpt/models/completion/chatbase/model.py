3a4c1a788536fc87bf893392e1748bb751f4e9f4
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-05-20 12:04:09 +0600

    Update model.py

--- opengpt/models/completion/chatbase/model.py
+++ opengpt/models/completion/chatbase/model.py
@@ -1,11 +1,27 @@
-import requests
+import aiohttp
+import asyncio
+
 class Model:
     captcha_code = "hadsa"
-    def GetAnswer(self, prompt: str, model: str = "gpt-4"):
+
+    async def get_answer(self, prompt: str, model: str = "gpt-4"):
         if model == "gpt-4":
             chat_id = "quran---tafseer-saadi-pdf-wbgknt7zn"
         elif model == "gpt-3.5-turbo":
             chat_id = "chatbase--1--pdf-p680fxvnm"
         messages = [{"role": "system", "content": ""}, {"role": "user", "content": prompt}]
-        r = requests.post("https://www.chatbase.co/api/fe/chat", json={"chatId": chat_id, "captchaCode": self.captcha_code, "messages": messages}).text
-        return r
+
+        async with aiohttp.ClientSession() as session:
+            response = await session.post(
+                "https://www.chatbase.co/api/fe/chat",
+                json={"chatId": chat_id, "captchaCode": self.captcha_code, "messages": messages}
+            )
+            result = await response.text()
+
+        return result
+   
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+    
