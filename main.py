7b4e262fbd3a246d62b666845b64526930cce709
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-05-26 14:57:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -449,17 +449,13 @@ def detect_replit():
 \033[1;33mPlease note that the .env file cannot exist on Replit.
 Instead, create environment variables (e.g., DISCORD_TOKEN and HUGGING_FACE_API) in the "Secrets" tab under "Tools".\033[0m""")
         from flask import Flask
-
         app = Flask("keepalive")
-
         @app.route('/', methods=['GET', 'POST', 'CONNECT', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'TRACE', 'HEAD'])
         def main():
             return 'Replit: Where code comes to life, dreams take shape, and possibilities are infinite'
-
         app.run(host='0.0.0.0', port=3000, debug=False, use_reloader=False)
     else:
         return
-
 detect_replit()
 
 bot.run(TOKEN)
