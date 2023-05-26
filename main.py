170288e6ff7e615cfd8fa7f36b9e9ae0a824a6f7
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-05-26 18:52:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from collections import deque
 from discord import Embed, Colour, app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+import replit_detector
 
 load_dotenv()
 
@@ -443,20 +444,5 @@ async def help(ctx):
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send("You do not have permission to use this command.")
-
-def detect_replit():
-    if os.path.isfile("replit.nix") or os.path.isfile(".replit"):
-        print("""\033[1;31m ⚠️ Warning: Looks like you are running this project on Replit\033[0m
-\033[1;33mPlease note that the .env file cannot exist on Replit.
-Instead, create environment variables (e.g., DISCORD_TOKEN and HUGGING_FACE_API) in the "Secrets" tab under "Tools".\033[0m""")
-        from flask import Flask
-        app = Flask("keepalive")
-        @app.route('/', methods=['GET', 'POST', 'CONNECT', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'TRACE', 'HEAD'])
-        def main():
-            return 'Replit: Where code comes to life, dreams take shape, and possibilities are infinite'
-        app.run(host='0.0.0.0', port=3000, debug=False, use_reloader=False)
-    else:
-        return
-detect_replit()
 
 bot.run(TOKEN)
