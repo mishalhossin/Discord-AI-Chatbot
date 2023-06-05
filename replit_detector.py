8ef74913719c3a53c09075bef0a785e5c5f1c227
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-06-05 07:14:39 +0600

    Update replit_detector.py

--- replit_detector.py
+++ replit_detector.py
@@ -52,23 +52,25 @@ cli.show_server_banner = lambda *x: None
 def run_flask_app():
     app.run(host='0.0.0.0', port=3000, debug=False, use_reloader=False)
 
-def detect_replit():
-    if "REPL_OWNER" in os.environ:
-        print(f"""\033[1;31m⚠️ Looks like you are running this project on Replit\033[0m
+Welcomer = f"""\033[1;31m⚠️ Looks like you are running this project on Replit\033[0m
         
 \033[1;33mPlease note that the .env file cannot exist on Replit.
 Instead, create environment variables (e.g., DISCORD_TOKEN and HUGGING_FACE_API) in the "Secrets" tab under "Tools".\033[0m
 
 \033[1;32mTake care {os.environ['REPL_OWNER']} ❤️\033[0m
-""")
+"""
+            
+def detect_replit():
+    if "REPL_OWNER" in os.environ:
         return True
     return False
 
-def detect_and_run():
+def detect_replit_and_run():
   if detect_replit() is True:
     threading.Thread(target=run_flask_app).start()
+    print(Welcomer)
   else:
     return
 
 if __name__ == "__main__":
-    detect_and_run()
+    detect_replit_and_run()
