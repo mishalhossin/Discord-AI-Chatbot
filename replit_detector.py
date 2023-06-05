1e9dbfd6d098181408f42ae78becf48120c89112
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-06-05 06:50:18 +0600

    Update replit_detector.py

--- replit_detector.py
+++ replit_detector.py
@@ -1,4 +1,4 @@
-import os
+from os import environ
 import threading
 from flask import Flask
 
@@ -12,16 +12,16 @@ def run_flask_app():
 
     app.run(host='0.0.0.0', port=3000, debug=False, use_reloader=False)
 
-
 def detect_replit():
-    if os.path.isfile("replit.nix") or os.path.isfile(".replit"):
-        print("""\033[1;31m ⚠️ Warning: Looks like you are running this project on Replit\033[0m
+    if "REPL_OWNER" in environ:
+        print(f"""\033[1;31m ⚠️ Warning: Looks like you are running this project on Replit\033[0m
 \033[1;33mPlease note that the .env file cannot exist on Replit.
-Instead, create environment variables (e.g., DISCORD_TOKEN and HUGGING_FACE_API) in the "Secrets" tab under "Tools".\033[0m""")
+Instead, create environment variables (e.g., DISCORD_TOKEN and HUGGING_FACE_API) in the "Secrets" tab under "Tools".\033[0m
+
+\033[1;32mTake care {environ['REPL_OWNER']} ❤️\033[0m""")
         threading.Thread(target=run_flask_app).start()
         return True
     return False
-
 
 if __name__ == "__main__":
     detect_replit()
