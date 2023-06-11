7eb02582c2c53e573b0f1c8fcc77f4fd894b2937
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-06-11 18:43:27 +0600

    Add retry for bad gate way error while making request

--- imaginepy/async_imagine.py
+++ imaginepy/async_imagine.py
@@ -2,6 +2,7 @@ import io
 
 import aiohttp
 from langdetect import detect
+import asyncio
 
 from .constants import *
 
@@ -70,28 +71,33 @@ class AsyncImagine:
         except Exception as e:
             print(f"An error occurred while validating cfg: {e}")
             return None
-
-        try:
-            async with self.session.post(
-                    url=f"{self.api}/sdprem",
-                    data={
-                        "model_version": self.version,
-                        "prompt": prompt + (style.value[3] or ""),
-                        "negative_prompt": negative or "",
-                        "style_id": style.value[0],
-                        "width": ratio.value[0],
-                        "height": ratio.value[1],
-                        "seed": seed or "",
-                        "steps": steps or "30",
-                        "cfg": validated_cfg,
-                        "priority": priority or "0",
-                        "high_res_results": high_res_results or "0"
-                    }
-            ) as resp:
-                return await resp.read()
-        except Exception as e:
-            print(f"An error occurred while making the request: {e}")
-            return None
+    
+        for attempt in range(2):
+            try:
+                async with self.session.post(
+                        url=f"{self.api}/sdprem",
+                        data={
+                            "model_version": self.version,
+                            "prompt": prompt + (style.value[3] or ""),
+                            "negative_prompt": negative or "",
+                            "style_id": style.value[0],
+                            "width": ratio.value[0],
+                            "height": ratio.value[1],
+                            "seed": seed or "",
+                            "steps": steps or "30",
+                            "cfg": validated_cfg,
+                            "priority": priority or "0",
+                            "high_res_results": high_res_results or "0"
+                        }
+                ) as resp:
+                    return await resp.read()
+            except Exception as e:
+                print(f"An error occurred while making the request: {e}")
+                if attempt == 0:
+                    await asyncio.sleep(0.5)
+                    print("Retrying...")
+                else:
+                    return None
 
     async def upscale(self, image: bytes) -> bytes:
         """Upscales the image."""
