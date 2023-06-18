e31eb49110ec081583fabf3bb97d37d75ab58152
mishalhossin <mishalhossin20@gmail.com>
2023-06-18 16:52:09 +0600

    Revert some stuff

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import io
 from itertools import cycle
 import datetime
-import sys
 
 import aiohttp
 import discord
@@ -510,16 +509,6 @@ if detect_replit():
     run_flask_in_thread()
 if update_on_start_up:
     from updater import update_repository
-    import asyncio
-
-    async def update_and_restart():
-        is_updated = await update_repository()
-        if is_updated:
-            print("Trying to restart main.py...")
-            python = sys.executable
-            os.execl(python, python, *sys.argv)
-
-    asyncio.run(update_and_restart())
-
-if __name__ == "__main__":
-    bot.run(TOKEN)
+    update_repository()
+    
+bot.run(TOKEN)
--- updater.py
+++ updater.py
@@ -1,54 +1,33 @@
-import asyncio
-import aiohttp
-import hashlib
 import os
 import shutil
-import zipfile
+import requests
+from zipfile import ZipFile
+from tqdm import tqdm
+import hashlib
 
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
 RESET = "\033[0m"
 
-async def download_file(session, url, destination):
-    async with session.get(url) as response:
-        with open(destination, "wb") as f:
-            while True:
-                chunk = await response.content.read(1024)
-                if not chunk:
-                    break
-                f.write(chunk)
-
-async def update_repository():
+def update_repository():
     repo_url = "https://github.com/mishalhossin/Discord-AI-Chatbot/archive/main.zip"
     excluded_files = ["config.yml", "channels.txt", ".env"]
+    response = requests.get(repo_url, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 1024  # 1 KB
+    progress_bar = tqdm(total=total_size, unit="B", unit_scale=True)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(repo_url) as response:
-            total_size = int(response.headers.get("Content-Length", 0))
-            downloaded_size = 0
-            block_size = 1024  # 1 KB
-            progress_bar = "["
+    with open("repository.zip", "wb") as zip_file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            zip_file.write(data)
 
-            with open("repository.zip", "wb") as zip_file:
-                while True:
-                    chunk = await response.content.read(block_size)
-                    if not chunk:
-                        break
-                    downloaded_size += len(chunk)
-                    if total_size > 0:
-                        progress = int(downloaded_size / total_size * 20)
-                    else:
-                        progress = 0
-                    progress_bar += "#" * progress + "-" * (20 - progress) + "]"
-                    print(f"Downloading: {progress_bar}", end="\r")
+    progress_bar.close()
 
-    print("\nExtracting files...")
-
-    with zipfile.ZipFile("repository.zip", "r") as zip_ref:
+    with ZipFile("repository.zip", "r") as zip_ref:
         zip_ref.extractall("temp")
 
     changed_files = []
-
     for root, dirs, files in os.walk("temp/Discord-AI-Chatbot-main"):
         for file in files:
             file_path = os.path.join(root, file)
@@ -76,10 +55,7 @@ async def update_repository():
         print(GREEN + "The following files have been updated:" + RESET)
         for file in changed_files:
             print(YELLOW + "- " + file + RESET)
-        return True
     else:
         print("No files have been updated.")
-        return False
-
 if __name__ == "__main__":
-    asyncio.run(update_repository())
+    update_repository()
