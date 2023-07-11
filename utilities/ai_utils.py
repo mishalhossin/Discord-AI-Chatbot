513745160875d8608869cf53acf5bea31ce89d7d
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-07-11 06:47:22 +0600

    Better imagine command

--- utilities/ai_utils.py
+++ utilities/ai_utils.py
@@ -136,7 +136,7 @@ async def generate_job(prompt, seed=None):
     params = {
         'new': 'true',
         'prompt': f'{quote(prompt)}',
-        'model': 'anything-v4.5-pruned.ckpt [65745d25]',
+        'model': 'dreamshaper_6BakedVae.safetensors [114c8abb]',
         'negative_prompt': '(nsfw:1.5),verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.8),cross-eyed,sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair',
         'steps': '30',
         'cfg': '9.5',
