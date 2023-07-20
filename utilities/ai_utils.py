276583c515915c6a695a5796862d14c3596ec523
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-07-20 08:44:11 +0600

    Prodia with more model support

--- utilities/ai_utils.py
+++ utilities/ai_utils.py
@@ -129,47 +129,33 @@ async def dall_e_gen(prompt, size, num_images):
     return imagefileobjs
     
 
-async def generate_job(prompt, seed=None):
-    print("Got here too")
-    if seed is None:
-      seed = random.randint(10000, 99999)
-    
-    url = 'https://api.prodia.com/generate'
-    params = {
-        'new': 'true',
-        'prompt': f'{quote(prompt)}',
-        'model': 'dreamshaper_6BakedVae.safetensors [114c8abb]',
-        'negative_prompt': '(nsfw:1.5),verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.8),cross-eyed,sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair',
-        'steps': '30',
-        'cfg': '9.5',
-        'seed': f'{seed}',
-        'sampler': 'Euler',
-        'aspect_ratio': 'square'
-    }
-    headers = {
-        'authority': 'api.prodia.com',
-        'accept': '*/*',
-        'accept-language': 'en-US,en;q=0.6',
-        'dnt': '1',
-        'origin': 'https://app.prodia.com',
-        'referer': 'https://app.prodia.com/',
-        'sec-ch-ua': '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'sec-gpc': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-    }
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, headers=headers) as response:
-            data = await response.json()
-            return data['job']
-
-async def generate_image(prompt):
-    job_id = await generate_job(prompt)
+async def generate_image_prodia(prompt, model, sampler, seed, neg):
+    print("\033[1;32m(Prodia) Creating image for :\033[0m", prompt)
+    start_time = time.time()
+    async def create_job(prompt, model, sampler, seed, neg):
+        if neg is None:
+            negative = "(nsfw:1.5),verybadimagenegative_v1.3, ng_deepnegative_v1_75t, (ugly face:0.8),cross-eyed,sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, bad anatomy, DeepNegative, facing away, tilted head, {Multiple people}, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worstquality, low quality, normal quality, jpegartifacts, signature, watermark, username, blurry, bad feet, cropped, poorly drawn hands, poorly drawn face, mutation, deformed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, extra fingers, fewer digits, extra limbs, extra arms,extra legs, malformed limbs, fused fingers, too many fingers, long neck, cross-eyed,mutated hands, polar lowres, bad body, bad proportions, gross proportions, text, error, missing fingers, missing arms, missing legs, extra digit, extra arms, extra leg, extra foot, repeating hair, nsfw, [[[[[bad-artist-anime, sketch by bad-artist]]]]], [[[mutation, lowres, bad hands, [text, signature, watermark, username], blurry, monochrome, grayscale, realistic, simple background, limited palette]]], close-up, (swimsuit, cleavage, armpits, ass, navel, cleavage cutout), (forehead jewel:1.2), (forehead mark:1.5), (bad and mutated hands:1.3), (worst quality:2.0), (low quality:2.0), (blurry:2.0), multiple limbs, bad anatomy, (interlocked fingers:1.2),(interlocked leg:1.2), Ugly Fingers, (extra digit and hands and fingers and legs and arms:1.4), crown braid, (deformed fingers:1.2), (long fingers:1.2)"
+        else:
+            negative = neg
+        url = 'https://api.prodia.com/generate'
+        params = {
+            'new': 'true',
+            'prompt': f'{quote(prompt)}',
+            'model': model,
+            'negative_prompt': f"{negative}",
+            'steps': '100',
+            'cfg': '9.5',
+            'seed': f'{seed}',
+            'sampler': sampler,
+            'upscale': 'True',
+            'aspect_ratio': 'square'
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as response:
+                data = await response.json()
+                return data['job']
+            
+    job_id = await create_job(prompt, model, sampler, seed, neg)
     url = f'https://api.prodia.com/job/{job_id}'
     headers = {
         'authority': 'api.prodia.com',
@@ -178,11 +164,12 @@ async def generate_image(prompt):
 
     async with aiohttp.ClientSession() as session:
         while True:
-            await asyncio.sleep(0.3)
             async with session.get(url, headers=headers) as response:
                 json = await response.json()
                 if json['status'] == 'succeeded':
                     async with session.get(f'https://images.prodia.xyz/{job_id}.png?download=1', headers=headers) as response:
                         content = await response.content.read()
                         img_file_obj = io.BytesIO(content)
+                        duration = time.time() - start_time
+                        print(f"\033[1;34m(Prodia) Finished image creation\n\033[0mJob id : {job_id}  Prompt : ", prompt, "in", duration, "seconds.")
                         return img_file_obj
